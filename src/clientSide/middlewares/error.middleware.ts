f69779a06366b07fada41cd1ca4448c3f4c98911
Hubulbib <kamal.kurbanov2018@gmail.com>
2022-12-14 20:05:01 +0300

    renaming a folder with partitions

---
+++ src/clientSide/middlewares/error.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { ApiError } from "../sections/auth/exceptions/api.error";
+
+export default function (err, req: Request, res: Response) {
+  console.log(err);
+  if (err instanceof ApiError) {
+    return res
+      .status(err.status)
+      .json({ message: err.message, errors: err.errors });
+  }
+  return res.status(500).json({ message: "Непредвиденная ошибка" });
+}
