2ba542804f69c88b680928d96f9d3729f8c74a55
Hubulbib <kamal.kurbanov2018@gmail.com>
2022-12-21 02:52:50 +0300

    changed some functions

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 /* Import dependencies */
 
 import express from 'express'
-import {connect} from 'mongoose'
+import { connect } from 'mongoose'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import 'dotenv/config'
-
 /* Import dependencies */
+/* Import routers */
+import authRouter from './clientSide/sections/auth/routers/auth.router.js'
 
 /* Import routers */
+/* Import middlewares */
+import authMiddleware from './clientSide/sections/auth/middlewares/auth.middleware.js'
+import adminCheckMiddleware from './adminPanel/middlewares/adminCheck.middleware.js'
+/* Import middlewares */
+/* Import Admin routers */
+import AdminUserRouter from './adminPanel/sections/user/routers/user.router.js'
 
-
-
-/* Import routers */
-
+/* Import Admin routers */
 
 /* Constant declaration */
 
@@ -23,25 +27,28 @@ const ORIGIN_URL = process.env.ORIGIN_URL || ''
 
 /* Constant declaration */
 
-
 /* Initialization app */
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: ORIGIN_URL
-}))
+    origin: ORIGIN_URL,
+  }),
+)
+
+app.use('/api/auth', authRouter)
+app.use('/api/admin/users', [authMiddleware, adminCheckMiddleware], AdminUserRouter)
 
 /* Initialization app */
-
 
 /* App start */
 
 connect(MONGODB)
-    .then(() => app.listen(PORT, () => console.log(`Server has been started on ${PORT}`)))
-    .catch(err => console.log(err))
+  .then(() => app.listen(PORT, () => console.log(`Server has been started on ${PORT}`)))
+  .catch((err) => console.log(err))
 
 /* App start */
--- src/clientSide/sections/auth/schemas/user.schema.ts
+++ src/clientSide/sections/auth/schemas/user.schema.ts
@@ -1,15 +1,15 @@
-import { model, Schema } from "mongoose";
-import { IUser } from "../interfaces/user.interface";
+import { model, Schema } from 'mongoose'
+import { IUser } from '../interfaces/user.interface.js'
 
 const schema = new Schema<IUser>({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  name: { type: String, required: true },
-  surname: { type: String, required: true },
-  patronymic: { type: String },
-  department: { type: String, required: true },
-  accesses: { type: [String], required: true, default: [] },
-  role: { type: String, required: true, default: "user" },
-});
+  fullName: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  department: { type: String },
+  accesses: { type: [String], required: true, default: ['1'] },
+  role: { type: String, required: true, default: 'user' },
+})
 
-export default model("User", schema);
+export default model('User', schema)
